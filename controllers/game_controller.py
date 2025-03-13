3e69a98486ca72d3df5da918176d5b3e36a032aa
Kallu-A <lucas.aries@protonmail.com>
2025-03-13 00:07:14 -0400

    feat: add type & display

--- controllers/game_controller.py
+++ controllers/game_controller.py
@@ -10,4 +10,8 @@ game_controller = Blueprint("game_controller", __name__)
 def game():
     pokemon_self = Pokemon("Pikachu", 12, 57, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
                         XPDifficulty.EASY, PokemonType.ELECTRIC, PokemonType.NONE)
-    return render_template("pages/game.html", pokemon_self=pokemon_self)
+    pokemon_self.current_hp = 24
+    pokemon_op = Pokemon("Salamèche", 10, 30, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+                         XPDifficulty.EASY, PokemonType.FIRE, PokemonType.NONE)
+    pokemon_op.current_hp = 5
+    return render_template("pages/game.html", pokemon_self=pokemon_self, pokemon_op=pokemon_op)
