7f912c5667596d50e2b53ea3b199776e84bbe05b
Kallu-A <lucas.aries@protonmail.com>
2025-03-12 23:21:32 -0400

    chore: setup database

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from controllers.game_controller import game_controller
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route("/")
-def dashboard():
-    return render_template("pages/game.html")
+with app.app_context():
+    db.create_all()
 
+app.register_blueprint(game_controller)
 
 # Gestion de la page d'erreur 404 (Page non trouvée)
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("errors/404.html"), 404
-
 
 # Gestion de la page d'erreur 500 (Erreur interne du serveur)
 @app.errorhandler(500)
