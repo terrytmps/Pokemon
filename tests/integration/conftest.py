8e5ac1ae378dd201db1589064a45c9dba8c68f77
Terry <terry57tt@gmail.com>
2025-04-04 10:06:17 -0400

    test: ajout de la configuration des tests d'intégration

---
+++ tests/integration/conftest.py
@@ -0,0 +1,43 @@
+import pytest
+import os
+import tempfile
+from app import app as flask_app, db, initialize_database
+
+
+@pytest.fixture(scope="module")
+def app():
+    """Create a test client for the Flask application."""
+    db_fd, db_path = tempfile.mkstemp()
+
+    flask_app.config.update(
+        {
+            "TESTING": True,
+            "SQLALCHEMY_DATABASE_URI": f"sqlite:///{db_path}",
+            "SQLALCHEMY_TRACK_MODIFICATIONS": False,
+        }
+    )
+
+    with flask_app.app_context():
+        print(f"Using test database: {db_path}")
+        db.drop_all()
+        db.create_all()
+        initialize_database()
+
+    yield flask_app
+
+    with flask_app.app_context():
+        db.session.remove()
+        db.drop_all()
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
